dd07fe8961e34752222bbb058b092fd0c3a94bea
Henry Frankland <git@henry.franklandhome.co.uk>
2022-02-07 14:23:40 +0000

    updated to wait a second and then paste. limitation the active shell window takes over, need to click back in the area you want the script to apply to

--- clipboard_to_stroke.py
+++ clipboard_to_stroke.py
@@ -3,8 +3,8 @@ import pyclip
 from pyautogui import write
 import logging
 from sys import argv
-
-debug1 = 0
+from time import sleep
+debug1 = 1
 
 logging.basicConfig(filename=f'{argv[0]}.log', encoding='utf-8',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -16,4 +16,5 @@ if debug1: logging.info(f"Coppied: {clp_d}")
 #convert bit list to string
 clp_utf_8 = clp_d.decode('utf-8')
 if debug1: logging.info(f"Coppied conv to string: {clp_utf_8}")
+sleep(1)
 write(clp_utf_8)
