2bbf952c45977b9d9d1b5d43ca5607e5605fb118
raymondtliu <raymondliu@eda-solutions.com>
2023-02-28 10:51:23 +0000

    Fixed formatting and typos

--- clipboard_to_stroke.py
+++ clipboard_to_stroke.py
@@ -1,20 +1,30 @@
-#libs
-import pyclip
-from pyautogui import write
+"""A script used to send keyboard inputs to the computer from clipboard"""
 import logging
 from sys import argv
 from time import sleep
-debug1 = 1
+import pyclip
+from pyautogui import write
 
-logging.basicConfig(filename=f'{argv[0]}.log', encoding='utf-8',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+debug_switch = 0
+
+logging.basicConfig(filename=f'{argv[0]}.log',
+                    encoding='utf-8',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-if debug1: logging.info("//////////////Script start//////////////")
+if debug_switch:
+    logging.info("//////////////Script start//////////////")
+
 #get clipboard
 clp_d = pyclip.paste()
-if debug1: logging.info(f"Coppied: {clp_d}")
+if debug_switch:
+    logging.info(f"Copied: {clp_d}")
+
 #convert bit list to string
 clp_utf_8 = clp_d.decode('utf-8')
-if debug1: logging.info(f"Coppied conv to string: {clp_utf_8}")
+if debug_switch:
+    logging.info(f"Copied conv to string: {clp_utf_8}")
 sleep(1)
 write(clp_utf_8)
